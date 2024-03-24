5ee77f7ca04e556b53a8c6eada6a8e7712381f9c
John Jorjill <orgil9506@gmail.com>
2024-03-23 20:25:02 -0500

    add quill

--- src/components/edit-note/editnote.tsx
+++ src/components/edit-note/editnote.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
 import {
   addNote,
@@ -7,7 +9,9 @@ import {
   selectNoteByTitle,
 } from "../../slices/notesSlice";
 import "./editnote.less";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
 interface EditNoteProps {
   note?: note;
@@ -15,34 +19,52 @@ interface EditNoteProps {
 
 export const EditNote: React.FC<EditNoteProps> = ({ note }) => {
   const dispatch = useDispatch();
+  const quillRef = useRef<Quill | null>(null);
   const selectedSkill = useSelector(
     (state: any) => state.skills.selectedSkill.title
   );
-  const [noteTitle, setNoteTitle] = useState("");
-  const [noteContent, setNoteContent] = useState("");
+  const [noteTitle, setNoteTitle] = useState(note?.notes_title || "");
+  const [noteContent, setNoteContent] = useState(note?.content || "");
+
+  useEffect(() => {
+    if (quillRef.current == null) {
+      quillRef.current = new Quill("#editor", {
+        theme: "snow", // this needs to match whatever theme you want from Quill
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, false] }],
+            ["bold", "italic", "underline"],
+            ["image", "code-block"],
+          ],
+        },
+      });
+
+      quillRef.current.on("text-change", function () {
+        // This is where you would handle data from the editor
+        // For example, by updating component state
+      });
+    }
+
+    // Set initial content if editing an existing note
+    if (note && note.content && quillRef.current) {
+      quillRef.current.root.innerHTML = note.content;
+    }
+  }, [note]);
 
   return (
     <div className="addnote-component">
       <div className="addnote">
         <input
           type="text"
-          value={note?.notes_title}
-          name=""
-          id=""
-          onChange={(t) => {
-            setNoteTitle(t.target.value);
-          }}
+          value={noteTitle}
+          onChange={(e) => setNoteTitle(e.target.value)}
         />
-        <textarea
-          placeholder="Write notes here"
-          value={note?.content}
-          onChange={(t) => {
-            setNoteContent(t.target.value);
-          }}
-        />
+        <div id="editor" style={{ height: "200px" }}></div>{" "}
+        {/* This is where Quill will attach */}
         <div
           className="create-note-button"
           onClick={() => {
+            // dispatch(updateNote({ ...note, content: quillRef.current?.root.innerHTML }));
             dispatch(deselectEditNoteMode());
           }}
         >
