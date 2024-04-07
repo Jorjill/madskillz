97f021791268e14bbac5afceac46cfb49b68f062
Jorjill <orgil9506@gmail.com>
2024-04-07 18:57:50 -0500

    test component

--- src/components/test/test.tsx
+++ src/components/test/test.tsx
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import "./test.less";
 
-export const Test:React.FC = () => {
-    return(
-        <div className="test-container">
-            <div className="">
-                test
-            </div>
-        </div>
-    );
-}
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Paris", isCorrect: true },
+      { text: "London", isCorrect: false },
+      { text: "Berlin", isCorrect: false },
+      { text: "Madrid", isCorrect: false },
+    ],
+  },
+];
+
+export const Test: React.FC = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [showNext, setShowNext] = useState(false);
+
+  const handleAnswerClick = (isCorrect: boolean) => {
+    if (isCorrect) {
+      console.log("Correct answer");
+    } else {
+      console.log("Incorrect answer");
+    }
+    setShowNext(true);
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      console.log("End of the test");
+    }
+    setShowNext(false);
+  };
+
+  return (
+    <div className="test-container">
+      <div className="question">{questions[currentQuestionIndex].question}</div>
+      <div className="answers">
+        {questions[currentQuestionIndex].answers.map((answer, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswerClick(answer.isCorrect)}
+          >
+            {answer.text}
+          </button>
+        ))}
+      </div>
+      {showNext && (
+        <button className="next-button" onClick={handleNextQuestion}>
+          Next
+        </button>
+      )}
+    </div>
+  );
+};
