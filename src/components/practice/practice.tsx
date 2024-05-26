49b7741312c63424ae391a49551a81a9eb209e35
Jorjill <orgil9506@gmail.com>
2024-05-26 11:24:08 -0500

    get general questions by skill

--- src/components/practice/practice.tsx
+++ src/components/practice/practice.tsx
@@ -1,25 +1,39 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./practice.less";
 import Quill from "quill";
 import { useDispatch, useSelector } from "react-redux";
 import { choosePage } from "../../slices/pageSlice";
 import {
-  selectRandomGeneralQuestion,
+  practiceThunks,
+  selectRandomGeneralQuestionBySkill,
   selectRandomPastQuestion,
   selectRandomSpecificQuestion,
-  selectRandomTestQuestion,
 } from "../../slices/practiceSlice";
 
 export const Practice: React.FC = () => {
   const dispatch = useDispatch();
   const quillRef = useRef<Quill | null>(null);
   const [answerContent, setAnswerContent] = useState("");
+  const selectedSkillTitle = useSelector(
+    (state: any) => state.skills.selectedSkill.title
+  );
   const practiceMode = useSelector((state: any) => state.practice.practiceMode);
-  const randomGeneralQuestion = useSelector(selectRandomGeneralQuestion);
+  const generalQuestions = useSelector(
+    (state: any) => state.practice.generalQuestions
+  );
+  const randomGeneralQuestion = useMemo(() => {
+    return selectRandomGeneralQuestionBySkill(
+      generalQuestions,
+      selectedSkillTitle
+    );
+  }, [generalQuestions, selectedSkillTitle]);
+
   const randomSpecificQuestion = useSelector(selectRandomSpecificQuestion);
   const randomPastQuestion = useSelector(selectRandomPastQuestion);
 
   useEffect(() => {
+    dispatch<any>(practiceThunks.fetchGeneralQuestions());
+
     if (quillRef.current === null) {
       // Only instantiate Quill if quillRef.current is null
       quillRef.current = new Quill("#editor", {
@@ -46,13 +60,13 @@ export const Practice: React.FC = () => {
     <div className="practice-container">
       <div className="practice">
         {practiceMode === "general" ? (
-          <h1>{randomGeneralQuestion}</h1>
+          <h1>{randomGeneralQuestion?.question}</h1>
         ) : practiceMode === "specific" ? (
           <h1>{randomSpecificQuestion}</h1>
         ) : practiceMode === "past" ? (
           <h1>{randomPastQuestion}</h1>
         ) : (
-        <div></div>
+          <div></div>
         )}
         <div id="editor" style={{ height: "500px" }}></div>{" "}
         <div
--- src/slices/practiceSlice.ts
+++ src/slices/practiceSlice.ts
@@ -1,34 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface generalQuestion {
+  question: String;
+  answer: String;
+  skill: String;
+}
 
 interface practiceState {
   practiceMode: String;
-  generalQuestions: String[];
+  generalQuestions: generalQuestion[];
   specificQuestions: String[];
   pastQuestions: String[];
   testQuestions: String[];
 }
 
 const initialState: practiceState = {
-    practiceMode: "general",
-    generalQuestions: ["general 1","general 2","general 3"],
-    specificQuestions: ["specific 1","specific 2","specific 3"],
-    pastQuestions: ["past 1","past 2","past 3"],
-    testQuestions: ["test 1","test 2","test 3"]
+  practiceMode: "general",
+  generalQuestions: [],
+  specificQuestions: ["specific 1", "specific 2", "specific 3"],
+  pastQuestions: ["past 1", "past 2", "past 3"],
+  testQuestions: ["test 1", "test 2", "test 3"],
 };
 
 const practiceSlice = createSlice({
   name: "practice",
   initialState,
   reducers: {
+    addGeneralQuestions: (state, actions) => {
+      state.generalQuestions = actions.payload;
+    },
     chooseMode: (state, actions) => {
       state.practiceMode = actions.payload;
     },
   },
 });
 
-const getRandomItem = (items: String[]) => {
+const getRandomItem = (items: any) => {
   const randomIndex = Math.floor(Math.random() * items.length);
   return items[randomIndex];
+};
+
+export const selectRandomGeneralQuestionBySkill = (
+  questions: any,
+  skill: string
+) => {
+  const filteredQuestions = questions.filter(
+    (question: any) => question.skill === skill
+  );
+  return getRandomItem(filteredQuestions);
 };
 
 export const selectRandomGeneralQuestion = (state: any) =>
@@ -43,7 +63,18 @@ export const selectRandomPastQuestion = (state: any) =>
 export const selectRandomTestQuestion = (state: any) =>
   getRandomItem(state.practice.testQuestions);
 
-export const selectPracticeMode = (state: any) =>
-  state.page.practiceMode;
+export const practiceThunks = {
+  fetchGeneralQuestions: () => async (dispatch: any) => {
+    // Fetch general questions from an API
+    console.log("fetching general questions");
+
+    const response = await axios.get("http://localhost:3000/general-question");
+    console.log(response.data);
+
+    dispatch(practiceSlice.actions.addGeneralQuestions(response.data));
+  },
+};
+
+export const selectPracticeMode = (state: any) => state.page.practiceMode;
 export const { chooseMode } = practiceSlice.actions;
 export default practiceSlice.reducer;
