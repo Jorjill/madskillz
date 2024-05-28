8c3185b13a65fe6f9161bc2567b8a2dda81c927a
Jorjill <orgil9506@gmail.com>
2024-05-27 23:08:20 -0500

    hardcode note

--- src/slices/notesSlice.ts
+++ src/slices/notesSlice.ts
@@ -23,7 +23,16 @@ const initialState: notesState = {
   isNoteSelected: false,
   isAddNoteMode: false,
   isEditNoteMode: false,
-  notes: [],
+  notes: [
+    {
+      id: "1",
+      notes_title: "React",
+      content: "React is a JavaScript library for building user interfaces",
+      noteSkill: "React",
+      datetime: "2021-09-01T00:00:00.000Z",
+      tags: ["JavaScript", "Frontend"],
+    },
+  ],
 };
 
 const notesSlice = createSlice({
