6bb9b11f468a5f42c1bbf1eeacdd36b6d072c29d
Jorjill <orgil9506@gmail.com>
2024-09-04 18:34:17 -0500

    fetch env vars for firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "madskillz-a6ba6.firebaseapp.com",
-  projectId: "madskillz-a6ba6",
-  storageBucket: "madskillz-a6ba6.appspot.com",
-  messagingSenderId: "925355319788",
-  appId: "1:925355319788:web:a55288510280ebdce860c9",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
