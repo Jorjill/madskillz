0e43e229785398d6ee764d8fedcd24542fcadf15
Jorjill <orgil9506@gmail.com>
2025-07-29 21:36:40 -0500

    simplify background

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#0a1929' }}>
       <Navigation />
       <Box
         component="main"
@@ -17,6 +17,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           p: 3,
           mt: 8, // Add margin top to account for the fixed AppBar
           width: '100%',
+          backgroundColor: '#0a1929',
         }}
       >
         {children}
