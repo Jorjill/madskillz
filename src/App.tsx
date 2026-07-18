53d2057c2a0e04e504ea32c36f00652852de9e8a
Jorjill <orgil9506@gmail.com>
2026-07-18 15:54:38 -0500

    home page scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AppRouter } from "./routers/AppRouter";
 
 function App() {
   return (
-    <div>
+    <div style={{ height: '100%' }}>
       <AuthProvider>
         <AppRouter />
       </AuthProvider>
